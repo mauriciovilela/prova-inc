23847f6db068927082aa7c31dc946b65fa9e2757
mauriciovilela <mauriciovilela@softbox.com.br>
2018-07-23 08:41:03 -0300

    correção cors

--- src/main/java/br/prova/zup/Config.java
+++ src/main/java/br/prova/zup/Config.java
@@ -8,10 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 public class Config extends WebMvcConfigurerAdapter {
-//  @Override
-//  public void addCorsMappings(CorsRegistry registry) {
-//    registry.addMapping("/**");
-//  }
+
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
